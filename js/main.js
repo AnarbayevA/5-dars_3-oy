c21277858474961cc137831b45a3140ef9be1642
AnarbayevA <azamat2000.com.com@gmail.com>
2022-02-21 22:27:54 +0300

    js

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var salom = "What is your name?"
+var a = prompt(salom)
+var b = alert("Hi " + a + " answer the following questions for use")
+var firstName = prompt("What is your first name?")
+var lastName = prompt("What is your last name?")
+var phone = prompt("What is your phone number?")
+var email = prompt("Plese, inter your email")
+var from = prompt("Whare are you from?")
+var father = prompt("What is your father's name?")
+var mather = prompt("What is your mather's name?")
+var hobbi = prompt("Your hobbi is ...")
+var car = prompt("What is the model of your car?")
+
+console.log(typeof firstName, firstName);
+console.log(typeof lastName, lastName);
+console.log(typeof phone, phone);
+console.log(typeof email, email);
+console.log(typeof from, from);
+console.log(typeof father, father);
+console.log(typeof hobbi, hobbi);
+console.log(typeof car, car);
